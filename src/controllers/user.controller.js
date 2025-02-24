02cb792f52977a4427ac6892ab381b605613d644
Abhishek Pundir <abhishek.pundir@appventurez.com>
2025-02-24 09:50:17 +0530

    worked on controller for user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,17 @@
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullName, email, username, password } = req.body;
 
-const registerUser = asyncHandler(async(req,res)=>{
-    res.status(200).json({
-        message:"ok"
-    })
-})
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() == "")
+  ) {
+    throw new ApiError();
+  }
+  console.log(email, "email");
+  res.status(200).json({
+    message: "ok",
+  });
+});
 
-export {registerUser}
+export { registerUser };
